7d6ba13f40394339a3dc5ed3d14c980085a28b42
lds <lds@lds.com>
2018-05-31 21:37:01 -0400

    04_Journal_app
        Starting version control is fun!

---
+++ journal.py
@@ -0,0 +1,43 @@
+"""
+this is the journal module
+"""
+
+import os
+
+
+def load(name):
+    """
+    this method creates and loads a new journal
+
+    :param name: the base name of the journal to load
+    :return: a new journal data structure populated with file data
+    """
+    data = []
+    filename = get_full_pathname(name)
+
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
+
+
+def save(name, journal_data):
+    filename = get_full_pathname(name)
+    print("....saving to: {} ".format(filename))
+
+    with open(filename, 'w') as fout:
+
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', 'journals', name + '.jrl'))
+    return filename
+
+
+def add_entry(text, journal_data):
+    journal_data.append(text)
+
---
+++ program2.py
@@ -0,0 +1,7 @@
+print('1.About to import program...')
+import program
+print('2. program imported')
+
+print('3. printing header')
+program.print_header()
+print('4. done!')
